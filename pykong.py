05293c5e56fd7cae4e296da26cbbd54074b55b4e
metamath1 <metamath@gmail.com>
2016-02-22 12:58:50 +0900

    add config.json

--- pykong.py
+++ pykong.py
@@ -1,23 +1,22 @@
 #!/usr/bin/env python
 
-import urllib2
+import urllib2, json
 import subprocess as sp
 
 def getadd(ch):
-	
     response = urllib2.urlopen("http://kong.kbs.co.kr/live_player/channelMini.php?id=kbs&channel=%s" % ch)
-	st = response.read()
-	
+    st = response.read()
     index1 = st.find("mms:")
     address = st[index1:]
-	
     return address
 
 def main():
-	#run_wsgi_app(application)
-    mms = getadd(1)
+    with open ("config.json", "r") as config_file:
+        config_json = config_file.read()
     
-    command = "C:/PROGRA~2/Daum/PotPlayer/PotPlayerMini "+ mms
+    config = json.loads(config_json)
+    mms = getadd(1)
+    command = config['player'] + ' ' + mms
     p = sp.Popen(command, stdout=sp.PIPE)
 
 if __name__ == "__main__":
