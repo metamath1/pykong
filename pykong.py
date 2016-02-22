6aa2fa5f3e520e4558588d8b2d788ca467b47066
metamath1 <metamath@gmail.com>
2016-02-22 13:16:46 +0900

    using getopt

--- pykong.py
+++ pykong.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 
-import urllib2, json
+import urllib2, json, getopt, sys
 import subprocess as sp
 
+def help():
+    print('USAGE: pykong -c [1|2] 1:KBS 1FM, 2:KBS 2FM')
+    
 def getadd(ch):
     response = urllib2.urlopen("http://kong.kbs.co.kr/live_player/channelMini.php?id=kbs&channel=%s" % ch)
     st = response.read()
@@ -15,7 +18,22 @@ def main():
         config_json = config_file.read()
     
     config = json.loads(config_json)
-    mms = getadd(1)
+    
+    #default is kbs classic fm
+    ch = 1
+    
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], 'c:', ['channel='])
+    except getopt.GetoptError as err:
+        print str(err)
+        help()
+        sys.exit(1)
+    
+    for opt, arg in opts :
+        if opt == '-c' or opt == '--channel':
+            ch = arg
+            
+    mms = getadd(ch)
     command = config['player'] + ' ' + mms
     p = sp.Popen(command, stdout=sp.PIPE)
 
