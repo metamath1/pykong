fb9b000e851dc3ad52a02982022d759a1ad8550b
metamath1 <metamath@gmail.com>
2016-02-23 13:52:28 +0900

    add a py2exe setup file

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+import py2exe
+
+
+Mydata_files = [('.', ['config.json'])]
+
+setup(
+    options = {
+                "py2exe": {
+                    "packages": ["encodings"],
+                    "bundle_files": 1
+                }
+            },
+    zipfile = None,
+    console = ["pykong.py"],
+    data_files = Mydata_files,
+)
